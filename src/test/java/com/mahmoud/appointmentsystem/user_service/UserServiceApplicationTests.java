15e1e84cc1963f2eb3176ba324c097cb417f87c2
Mahmoud-Ramadan2 <mahmoudramadan385@gmail.com>
2025-03-25 14:08:15 +0200

    Initial commit - user service  for Doctor Appointment System

---
+++ src/test/java/com/mahmoud/appointmentsystem/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmoud.appointmentsystem.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
